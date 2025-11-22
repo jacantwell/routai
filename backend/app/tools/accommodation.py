9bc0b452f0df59a5165bd70d40942f2e1d32068e
Jasper Cantwell <jasper66018@gmail.com>
2025-11-22 08:02:28 +0000

    New tool for fetching accommodation in a given area

---
+++ backend/app/tools/accommodation.py
@@ -0,0 +1,82 @@
+from langchain.tools import tool
+from pydantic import BaseModel
+from pydantic_extra_types.coordinate import Coordinate
+
+import requests
+import os
+
+from app.config.config import settings
+
+GOOGLE_PLACES_API_ENDPOINT = (
+    "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+)
+
+
+class Accommodation(BaseModel):
+    name: str
+    address: str
+    map_link: str
+    rating: float
+
+
+@tool
+def get_accommodation(location: Coordinate, radius: int = 5) -> list[Accommodation]:
+    """Find accommodation options within a given radius around a given location
+
+    Args:
+        location: The location to search near
+        radius: Radius, in km, around which to search
+    """
+
+    # Convert radius from km to meters (Google API uses meters)
+    radius_meters = radius * 1000
+
+    # Extract latitude and longitude from Coordinate
+    lat = location.latitude
+    lon = location.longitude
+
+    location_area = {
+        "circle": {
+            "center": {"latitude": location.latitude, "longitude": location.longitude},
+            "radius": radius_meters,
+        }
+    }
+
+    params = {
+        "locationRestriction": location_area,
+        "regionCode": "GB",
+        "includedTypes": ["lodging"],
+        "maxResultCount": 5,
+    }
+
+    headers = {
+        "Content-Type": "application/json",
+        "X-Goog-Api-Key": settings.GOOGLE_API_KEY,
+        "X-Goog-FieldMask": "routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline",
+    }
+
+    try:
+        # Make the request
+        response = requests.get(
+            GOOGLE_PLACES_API_ENDPOINT, params=params, headers=headers
+        )
+        response.raise_for_status()
+
+        data = response.json()
+
+        accommodation_data = data.get("places", [])
+        results = [
+            Accommodation(
+                name=acom["displayName"],
+                address=acom["formattedAddress"],
+                map_link=acom["googleMapsUri"],
+                rating=acom["rating"],
+            )
+            for acom in accommodation_data
+        ]
+    except requests.exceptions.RequestException as e:
+        raise Exception(f"Error making request to Google Places API: {str(e)}")
+    except Exception as e:
+        raise e
+    
+    return results
