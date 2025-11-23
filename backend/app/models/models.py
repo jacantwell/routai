d0df88973d657a00f084a0b201c160448bd905e4
Jasper Cantwell <jasper66018@gmail.com>
2025-11-23 16:14:52 +0000

    Switching from waypoints to segments so we can save polylines for each day of cycling

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -1,20 +1,29 @@
-from decimal import Decimal
+from typing import Optional
 from pydantic import BaseModel
 from pydantic_extra_types.coordinate import Coordinate
+
 
 class Location(BaseModel):
     name: str
     coordinates: Coordinate
 
+
+class Accommodation(BaseModel):
+    name: str
+    address: str
+    map_link: str
+    rating: Optional[float]
+
+
 class Route(BaseModel):
     polyline: str
+    origin: Coordinate
+    destination: Coordinate
     distance: int
-    duration: str
+    elevation_gain: int
 
 
-class Waypoint(BaseModel):
+class Segment(BaseModel):
     day: int
-    coordinates: Coordinate
-    distance_from_origin: Decimal
-    segment_distance: Decimal
-
+    route: Route
+    accommodation_options: list[Accommodation] = []
